d9e2ad227e4f6ce47d6f10ef7d5d19e875c400be
rizky redjo <celebgramme.dev@gmail.com>
2022-07-19 14:08:57 +0700

    seed

--- database/seeders/ConfigTableSeeder.php
+++ database/seeders/ConfigTableSeeder.php
@@ -15,8 +15,62 @@ class ConfigTableSeeder extends Seeder
     public function run()
     {
         DB::table('compares')->insert([
-            'symbol_binance' => '',
-            'symbol_kucoin' => '',
+            'symbol_binance' => 'ADAUSDT',
+            'symbol_kucoin' => 'ADA-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'BNBUSDT',
+            'symbol_kucoin' => 'BNB-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'DOTUSDT',
+            'symbol_kucoin' => 'DOT-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'SUPERUSDT',
+            'symbol_kucoin' => 'SUPER-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'UNFIUSDT',
+            'symbol_kucoin' => 'UNFI-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'SUSHIUSDT',
+            'symbol_kucoin' => 'SUSHI-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'XLMUSDT',
+            'symbol_kucoin' => 'XLM-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'CAKEUSDT',
+            'symbol_kucoin' => 'CAKE-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'ALPHAUSDT',
+            'symbol_kucoin' => 'ALPHA-USDT',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]); 
+        DB::table('compares')->insert([
+            'symbol_binance' => 'COCOSUSDT',
+            'symbol_kucoin' => 'COCOS-USDT',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]); 

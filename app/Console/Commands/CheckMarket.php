ce5325da4ce448584978c347a7df33b923c31265
rizky redjo <celebgramme.dev@gmail.com>
2022-07-26 22:48:47 +0700

    fixing

--- app/Console/Commands/CheckMarket.php
+++ app/Console/Commands/CheckMarket.php
@@ -63,20 +63,24 @@ class CheckMarket extends Command
         foreach($compares as $compare){
           $counter += 1;
           echo $compare->symbol_kucoin." ".$compare->symbol_binance.";";
-          $price_kucoin = $this->check_kucoin($compare->symbol_kucoin);
-          $price_binance = $this->check_binance($compare->symbol_binance);
+          $arr_kucoin = $this->check_kucoin($compare->symbol_kucoin);
+          $arr_binance = $this->check_binance($compare->symbol_binance);
+          $price_kucoin = $arr_kucoin['price'];
+          $price_binance = $arr_binance['price'];
 
           $exchange= "";
           $symbol = "";
+          $max = max($price_kucoin, $price_binance);
+          $percentage = abs($price_kucoin - $price_binance) / $max * 100;
           if ($price_kucoin>$price_binance){
             $exchange= "binance";
             $symbol = $compare->symbol_kucoin;
+            $percentage = abs($arr_binance['price_ask'] - $arr_kucoin['price_bid']) / $max * 100;
           }else {
             $exchange= "kucoin";
             $symbol = $compare->symbol_binance;
+            $percentage = abs($arr_kucoin['price_ask'] - $arr_binance['price_ask']) / $max * 100;
           }
-          $max = max($price_kucoin, $price_binance);
-          $percentage = abs($price_kucoin - $price_binance) / $max * 100;
           echo $percentage;
           if ( ($percentage >= 1 ) && ($price_binance<>0 || $price_kucoin<>0) ) {
             echo "greater than 2";
@@ -125,13 +129,17 @@ class CheckMarket extends Command
       curl_close($ch);
       $output = json_decode($response,true);
       // print_r($output);
-      return $output['data']['price'];
+      return [
+        "price"=>$output['data']['price'],
+        "price_ask"=>$output['data']['bestAsk'],
+        "price_bid"=>$output['data']['bestBid'],
+      ];
     }
 
     public function check_binance($symbol){
       $curl = curl_init();
       $url = 'https://api2.binance.com/api/v3/ticker/price?symbol='.$symbol;
-      // $url = 'https://api1.binance.com/api/v3/ping';
+      //$url = 'https://api2.binance.com/api/v3/ticker/bookTicker?symbol='.$symbol;
       //$proxy = '127.0.0.1:8888';
 
       $ch = curl_init($url);
@@ -148,7 +156,37 @@ class CheckMarket extends Command
       $err = curl_error($ch);
       curl_close($ch);
       $output = json_decode($response,true);
-      return $output['price'];
+      $price = $output['price'];
+
+
+
+
+
+      $curl = curl_init();
+      //$url = 'https://api2.binance.com/api/v3/ticker/price?symbol='.$symbol;
+      $url = 'https://api2.binance.com/api/v3/ticker/bookTicker?symbol='.$symbol;
+      //$proxy = '127.0.0.1:8888';
+
+      $ch = curl_init($url);
+      //curl_setopt($ch, CURLOPT_PROXY, $proxy);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+      curl_setopt($ch, CURLOPT_VERBOSE, 0);
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 360);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+     
+      $response = curl_exec($ch);
+      $err = curl_error($ch);
+      curl_close($ch);
+      $output = json_decode($response,true);
+
+      return [
+        "price"=>$price,
+        "price_ask"=>$output['askPrice'],
+        "price_bid"=>$output['bidPrice'],
+      ];
 
     }
 /* end class */

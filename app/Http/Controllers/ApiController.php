1d7d896b531e1b5892171728b3f96bf068bf21b0
rizky redjo <celebgramme.dev@gmail.com>
2022-08-01 09:11:47 +0700

    testing api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Database\QueryException;
+use App\User;
+use Mail;
+use DB,Crypt,Browser;
+use Carbon\Carbon;
+use Binance\API as bapi;
+
+use Illuminate\Support\Facades\Storage;
+
+class ApiController extends Controller
+{
+    public function test_api(Request $request)
+    {
+      $api = new bapi("CIzCTFYY34Mu4ShFl6CaJ9Y575jyOIbfvFlTucRKZqr0XgnYeNU9T9YaYAQYuzPn","xzPomFHFEWhB4u6wINtkexG0MIQKy6itO72U8uMFwm4DXdhbTUAxGD9h8lscmPRn");
+      $balances = $api->balances();
+      dd($balances);
+      echo "a";
+    }
+    
+/* end class */
+
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test', [ApiController::class, 'test_api']);
